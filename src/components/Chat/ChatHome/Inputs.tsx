191094f17d6aba9e8e89f3cf84b4be59935e83f3
blazej-k <blazejkania1@gmail.com>
2021-04-15 19:13:34 +0200

    join to socket room when is new group of user

--- src/components/Chat/ChatHome/Inputs.tsx
+++ src/components/Chat/ChatHome/Inputs.tsx
@@ -4,73 +4,54 @@ import { useEffect } from 'react';
 import { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { createGroup } from '../../../actions/CommunityActions';
-import { joinToGroup as join } from '../../../actions/UserActions';
 import { useColor, useSocket } from '../../hooks/Hooks';
 
-// interface InputsProps {
-    
-// }
- 
+
 const Inputs: FC = () => {
 
     const [newFriend, setNewFriend] = useState('')
     const [newGroup, setNewGroup] = useState('')
 
     const dispatch = useDispatch()
-    const {userReducer, commReducer} = useSelector((state: Store) => state)
+    const { userReducer } = useSelector((state: Store) => state)
 
-    const {color} = useColor()
-    const {client} = useSocket()
+    const { color } = useColor()
+    const { client } = useSocket()
 
-    // useEffect(() => {
-    //     if (joinToGroup) {
-    //         const { community: { groups } } = commReducer
-    //         setJoinToGroup(false)
-    //         const { groupId } = groups[groups.length - 1]
-    //         const { groupName } = groups[groups.length - 1]
-    //         const group = {
-    //             groupId,
-    //             groupName,
-    //             members: [],
-    //             dialogues: []
-    //         }
-    //         dispatch(join(group, login, sex, 'accept'))
-    //         client.emit('join to group', groupId)
-    //     }
-    // }, [commReducer])
+    const { user: { login, sex, groups, groups: {length} } } = userReducer
+
+    const [userGroupsLength, setUserGroupsLength] = useState(length)
+
+    useEffect(() => {
+        if(length > userGroupsLength) {
+            setUserGroupsLength(length)
+            groups[length - 1]
+            client.emit('join to group', groups[length - 1].groupId)
+        }
+    }, [length])
 
     const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-        const {id, value} = e.target
-        if(id === 'friend'){
+        const { id, value } = e.target
+        if (id === 'friend') {
             setNewFriend(value)
         }
-        else{
+        else {
             setNewGroup(value)
         }
     }
-    
+
     const handleCreateGroup = () => {
-        dispatch(createGroup({groupName: newGroup, login, sex}))
+        dispatch(createGroup({ groupName: newGroup, login, sex }))
         setNewGroup('')
-        setClient()
     }
 
-    const setClient = () => {
-        if(error.length === 0) {
-            // console.log(commReducer)
-            // client.emit('join to group', groupId)
-        }
-    }
-
-    const {user: {login, sex}, error} = userReducer
- 
     return (
         <>
-            <input type="text" value={newFriend} id='friend' onChange={handleInput} placeholder='Add new friend'/>
-            <input type="text" value={newGroup} id='group' onChange={handleInput} placeholder='Create new group'/>
+            <input type="text" value={newFriend} id='friend' onChange={handleInput} placeholder='Add new friend' />
+            <input type="text" value={newGroup} id='group' onChange={handleInput} placeholder='Create new group' />
             <button className={color} onClick={handleCreateGroup}>Create!</button>
         </>
     );
 }
- 
+
 export default Inputs;
