c07f9f4399dd3ca3dc63be10b04d76329bbd0f6d
blazej-k <blazejkania1@gmail.com>
2021-05-08 19:52:53 +0200

    add metatags

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React, { FC } from 'react'
 import ReactDOM from 'react-dom'
 import Page from './components/Page'
 import SocketProvider from './components/context/SocketContext';
+import MetaTags from 'react-meta-tags';
 import './style/index.scss'
 import { Provider } from 'react-redux';
 import { store } from './reducers/store';
@@ -10,6 +11,16 @@ const App: FC = () => {
 
     return (
         <SocketProvider>
+            <MetaTags>
+                <title>ChatZilla</title>
+                <meta
+                    name='description' 
+                    content="Everyone can use this page to write with friends and groups! You have to only register. That's all!" />
+                <meta name='keywords' content='chat, friends, groups, communication, community, chatzilla' />
+                <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+                <meta name='author' content='Błazej Kania'/>
+                <meta charSet='UTF-8'/>
+            </MetaTags>
             <Provider store={store}>
                 <Page/>
             </Provider>
