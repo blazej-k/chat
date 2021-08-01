cba8078e203020efe312f79c64255326a7902f8e
blazej-k <blazejkania1@gmail.com>
2021-08-01 16:13:40 +0200

    update home e2e tests and some scripts in package.json

--- src/e2e/Home.e2e.test.ts
+++ src/e2e/Home.e2e.test.ts
@@ -11,43 +11,56 @@ afterAll(async () => {
     await browser.close()
 })
 
+afterEach(async() => {
+    await page.reload()
+})
+
 describe('Home e2e', () => {
     it('should open and close sign in modal', async () => {
         await page.click('#sign-in')
         await page.click('#cancel')
+        const modal = await page.$('.sign-in-modal')
+        expect(modal).toBeNull()
     })
     it('should open and close sign up modal', async () => {
         await page.click('#sign-up')
         await page.click('#cancel')
+        const modal = await page.$('.sign-in-modal')
+        expect(modal).toBeNull()
     })
     it('should redirect between modals', async () => {
         await page.click('#sign-in')
         await page.click('.redirect')
-        await page.waitForSelector('#cancel')
+        let modal = await page.$('.sign-in-modal')
+        expect(modal).toBeDefined()
         await page.click('#cancel')
+        modal = await page.$eval('.sign-in-modal', elem => elem)
+        expect(modal).toBeUndefined()
     })
-    it('should open chat', async () => {
+    it('should open chat page', async () => {
         await page.click('#sign-in')
         await page.waitForSelector('#login')
         await page.type('#login', 'blazej1')
         await page.type('#password', '1')
         await page.click('#submit')
         await page.waitForSelector('.chat-content-wrapper')
+        const chat = await page.$('.chat-content-wrapper')
+        expect(chat).toBeDefined()
     })
-    it('should show error', async () => {
-        await page.goto('http://localhost:8000')
+    it('should show error mess in modal', async () => {
         await page.click('#sign-in')
         await page.type('#login', 'blazej2')
         await page.type('#password', '1')
         await page.click('#submit')
-        await page.waitForSelector('.user-auth-error')
+        const errorMess = await page.$eval('.user-auth-error', elem => elem.textContent)
+        expect(errorMess).toBe('Invalid login or password')
     })
     it('should show warning when some input is empty', async () => {
-        await page.goto('http://localhost:8000')
         await page.click('#sign-in')
         await page.type('#login', 'blazej2')
         await page.click('#submit')
         await page.waitForSelector('.form-validate')
+        const warningMess = await page.$eval('.form-validate', elem => elem.textContent)
+        expect(warningMess).toBe('Required')
     })
-
 })
